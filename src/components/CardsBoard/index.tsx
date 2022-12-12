aa30f0e95fa8def71bc8511a59cfd183eb2f14cb
Yulia Kulinich <yuliakulinichsoft@gmail.com>
2022-12-12 17:20:59 +0200

    Added using action creators to components

--- src/components/CardsBoard/index.tsx
+++ src/components/CardsBoard/index.tsx
@@ -2,82 +2,17 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
+import ICard from '../../interfaces/ICard';
 import Card from '../Card/index';
 
-// TODO get rid of mocked data
-const cards = [
-  {
-    id: 1,
-    city: {
-      id: 710202,
-      name: 'Dmitrivka',
-      state: '',
-      country: 'UA',
-      coord: {
-        lon: 24.62466,
-        lat: 48.52354,
-      },
-    },
-  },
-  {
-    id: 2,
-    city: {
-      id: 710202,
-      name: 'Dnipro',
-      state: '',
-      country: 'UA',
-      coord: {
-        lon: 24.62466,
-        lat: 48.52354,
-      },
-    },
-  },
-  {
-    id: 3,
-    city: {
-      id: 710202,
-      name: 'London',
-      state: '',
-      country: 'UA',
-      coord: {
-        lon: 24.62466,
-        lat: 48.52354,
-      },
-    },
-  },
-  {
-    id: 4,
-    city: {
-      id: 710202,
-      name: 'Paris',
-      state: '',
-      country: 'UA',
-      coord: {
-        lon: 24.62466,
-        lat: 48.52354,
-      },
-    },
-  },
-  {
-    id: 5,
-    city: {
-      id: 710202,
-      name: 'Not Paris',
-      state: '',
-      country: 'UA',
-      coord: {
-        lon: 24.62466,
-        lat: 48.52354,
-      },
-    },
-  },
-];
+export interface ICardsBoard {
+  cards: ICard[];
+}
 
-const CardsBoard = () => {
-  const deleteCard = (idCard: number): void => {
-    console.log(`delete ${idCard}`);
-  };
-  const updateCard = (idCard: number): void => {
+const CardsBoard = (props: ICardsBoard) => {
+  const { cards } = props;
+
+  const updateCard = (idCard: string): void => {
     console.log(`update ${idCard}`);
   };
 
@@ -92,7 +27,7 @@ const CardsBoard = () => {
       ) : (
         <Grid container spacing={1} minHeight="100vh">
           {cards.map((card) => (
-            <Card card={card} deleteCard={deleteCard} updateCard={updateCard} key={card.id} />
+            <Card card={card} updateCard={updateCard} key={card.id} />
           ))}
         </Grid>
       )}
--- src/components/CityNavigation/index.tsx
+++ src/components/CityNavigation/index.tsx
@@ -3,18 +3,23 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 
+import useAppDispatch from '../../hooks/useAppDispatch';
 import SearchInput from '../SearchInput';
 import ICity from '../../interfaces/ICity';
 import citiesData from '../../data/city.list.json';
+import * as actions from '../../containers/WeatherSearcherPage/redux/actionCreators/cards';
 
 const CityNavigation = () => {
   const cities = citiesData as Array<ICity>;
   const [cityId, setCityId] = useState<number>();
   console.log(cityId);
 
-  const handleCitySubmit = (): void => {
+  const dispatch = useAppDispatch();
+
+  const handleCitySubmit = async (): Promise<void> => {
     if (cityId) {
-      console.log('Go action');
+      // TODO check if cards is already created:
+      await dispatch(actions.addCard(cityId));
     }
   };
 

efb2f4d0cfe77da48908b98902d4d49f1147e95d
Yulia Kulinich <yuliakulinichsoft@gmail.com>
2022-12-12 17:12:38 +0200

    Added card reducer

---
+++ src/containers/WeatherSearcherPage/redux/reducer.ts
@@ -0,0 +1,34 @@
+import initialState, { IWeatherSearcherPageState } from './initialState';
+import IWeatherSearcherActionTypes from './actionTypes/actionsTypes';
+import actionTypes from './actionTypes/actionTypesNames';
+
+const weatherSearcherReducer = (
+  state: IWeatherSearcherPageState = initialState,
+  action: IWeatherSearcherActionTypes,
+): IWeatherSearcherPageState => {
+  console.log(action.type);
+  console.log(action.payload);
+  switch (action.type) {
+    case actionTypes.CARD_ADD_SUCCESS:
+      return {
+        ...state,
+        errorMessage: null,
+        cards: [...state.cards, action.payload.card],
+      };
+    case actionTypes.CARD_DELETE_REQUEST:
+      return {
+        ...state,
+        errorMessage: null,
+        cards: state.cards && state.cards.filter((card) => card.id !== action.payload.cardId), // TODO change used new JS version
+      };
+    case actionTypes.CARD_ADD_ERROR:
+    case actionTypes.CARD_DELETE_ERROR:
+      return {
+        ...state,
+        errorMessage: action.payload.errorMessage,
+      };
+    default:
+      return state;
+  }
+};
+export default weatherSearcherReducer;

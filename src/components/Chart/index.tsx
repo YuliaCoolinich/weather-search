e3a9b7700cd9917f6cc42a1dc35cf43ab6af9850
Yulia Kulinich <yuliakulinichsoft@gmail.com>
2022-12-15 01:27:34 +0200

    Added Chart component for forecasts

---
+++ src/components/Chart/index.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import {
+  Chart,
+  ChartCategoryAxis,
+  ChartCategoryAxisItem,
+  ChartSeries,
+  ChartSeriesItem,
+  ChartSeriesLabels,
+  ChartSeriesItemTooltip,
+} from '@progress/kendo-react-charts';
+
+import 'hammerjs';
+import IChartDataItem from '../../interfaces/IChartDataItem';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const pointColor = (point: any) => {
+  if (point.value >= 0) {
+    return 'green';
+  } else {
+    return 'blue';
+  }
+};
+
+export interface IChartContainer {
+  data: Array<IChartDataItem>;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const labelContent = (e: any) => `${e.value.toFixed(0)} °C`;
+
+const ChartContainer = (props: IChartContainer): JSX.Element => {
+  const { data } = props;
+  return (
+    <>
+      {!data ? null : (
+        <Box style={{ height: '300px' }}>
+          <Chart style={{ height: '300px', width: '100%', marginTop: '50px', padding: '30px' }}>
+            <ChartSeries>
+              <ChartSeriesItem
+                type="line"
+                data={data}
+                color={pointColor}
+                field="temp"
+                categoryField="time"
+                highField="100px"
+              >
+                <ChartSeriesLabels color="black" position="above" content={labelContent} />
+                <ChartSeriesItemTooltip color="black" />
+              </ChartSeriesItem>
+            </ChartSeries>
+
+            <ChartCategoryAxis>
+              <ChartCategoryAxisItem title={{ text: 'Time', color: 'black' }} />
+            </ChartCategoryAxis>
+          </Chart>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default ChartContainer;
